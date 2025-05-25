96894a0770cb0dbdb0637bf32eac067467edcc8f
Vladislav Perelygin <perelyginvladislav02@gmail.com>
2025-05-26 02:40:35 +0300

    Реализовал игровую логикуи игровой экран

--- RahooCoTestApp/Modules/Game/GameScene.swift
+++ RahooCoTestApp/Modules/Game/GameScene.swift
@@ -5,3 +5,22 @@
 //  Created by Владислав Перелыгин on 25/05/2025.
 //
 
+import UIKit
+import SpriteKit
+
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        setupGameBackground()
+    }
+    
+    private func setupGameBackground() {
+        let background = SKSpriteNode(imageNamed: "Bg_2")
+        background.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        background.zPosition = 0
+        background.size = CGSize(width: self.size.width * 2, height: self.size.height)
+        addChild(background)
+    }
+}
