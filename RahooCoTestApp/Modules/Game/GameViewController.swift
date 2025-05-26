ee66db28326eb05de2b179b03db5b042806dfb70
Vladislav Perelygin <perelyginvladislav02@gmail.com>
2025-05-26 18:52:08 +0300

    Правки по UI

--- RahooCoTestApp/Modules/Game/GameViewController.swift
+++ RahooCoTestApp/Modules/Game/GameViewController.swift
@@ -12,7 +12,6 @@ import AVFoundation
 
 class GameViewController: UIViewController {
 
-    // MARK: - UI Components
     private let viewModel: GameViewModel
     let skView = SKView()
     private let gridView = UIView()
@@ -22,30 +21,29 @@ class GameViewController: UIViewController {
     private var isPaused = false
 
     private let settingButton: UIButton = {
-        let setting = UIButton()
-        setting.setImage(UIImage(named: "settingButton"), for: .normal)
-        return setting
-    }()
-    
-    private let pauseButton: UIButton = {
-       let pause = UIButton()
-        pause.setImage(UIImage(named: "pauseButton"), for: .normal)
-        return pause
-    }()
-    
-    private let backButton: UIButton = {
-        let back = UIButton()
-        back.setImage(UIImage(named: "leftButton"), for: .normal)
-        return back
-    }()
-    
-    private let refreshButton: UIButton = {
-        let refresh = UIButton()
-        refresh.setImage(UIImage(named: "refreshButton"), for: .normal)
-        return refresh
+        let button = UIButton()
+        button.setImage(UIImage(named: "settingButton"), for: .normal)
+        return button
     }()
 
-    // MARK: - Init
+    private let pauseButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "pauseButton"), for: .normal)
+        return button
+    }()
+
+    private let backButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "leftButton"), for: .normal)
+        return button
+    }()
+
+    private let refreshButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "refreshButton"), for: .normal)
+        return button
+    }()
+
     init(viewModel: GameViewModel = GameViewModel()) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -55,7 +53,6 @@ class GameViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
-    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSKView()
@@ -79,82 +76,69 @@ class GameViewController: UIViewController {
         }
     }
 
-    // MARK: - Setup
     private func setupSKView() {
         view.addSubview(skView)
         skView.snp.makeConstraints { $0.edges.equalToSuperview() }
     }
 
-    
-
     private func setupButtons() {
         view.addSubview(settingButton)
         settingButton.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(120)
-            $0.left.equalToSuperview().offset(25)
-            $0.width.equalTo(45)
-            $0.height.equalTo(45)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            $0.leading.equalToSuperview().offset(20)
+            $0.width.height.equalTo(45)
         }
-        
+
         gridView.backgroundColor = .clear
         view.addSubview(gridView)
         gridView.snp.makeConstraints {
-            $0.center.equalToSuperview()
+            $0.top.equalTo(settingButton.snp.bottom).offset(100)
+            $0.centerX.equalToSuperview()
             $0.width.equalTo(view.snp.width).multipliedBy(0.9)
             $0.height.equalTo(gridView.snp.width)
         }
-        
-        view.addSubview(pauseButton)
-        pauseButton.snp.makeConstraints {
-            $0.top.equalTo(gridView.snp.bottom).offset(115)
-            $0.left.equalToSuperview().offset(25)
-            $0.width.equalTo(45)
-            $0.height.equalTo(45)
+
+        setupCards()
+
+        let controlStack = UIStackView(arrangedSubviews: [pauseButton, backButton, refreshButton])
+        controlStack.axis = .horizontal
+        controlStack.spacing = 85
+        controlStack.distribution = .equalSpacing
+        view.addSubview(controlStack)
+
+        controlStack.snp.makeConstraints {
+            $0.top.equalTo(gridView.snp.bottom).offset(75)
+            $0.centerX.equalToSuperview()
         }
-        
-        view.addSubview(backButton)
-        backButton.snp.makeConstraints {
-            $0.top.equalTo(gridView.snp.bottom).offset(115)
-            $0.left.equalTo(pauseButton.snp.right).offset(100)
-            $0.width.equalTo(45)
-            $0.height.equalTo(45)
+
+        [pauseButton, backButton, refreshButton].forEach { button in
+            button.snp.makeConstraints {
+                $0.width.height.equalTo(45)
+            }
         }
-        
-        view.addSubview(refreshButton)
-        refreshButton.snp.makeConstraints {
-            $0.top.equalTo(gridView.snp.bottom).offset(115)
-            $0.left.equalTo(backButton.snp.right).offset(90)
-            $0.width.equalTo(45)
-            $0.height.equalTo(45)
-        }
-        
+
         settingButton.addTarget(self, action: #selector(settingTapped), for: .touchUpInside)
         backButton.addTarget(self, action: #selector(backTapped), for: .touchUpInside)
         pauseButton.addTarget(self, action: #selector(pauseTapped), for: .touchUpInside)
         refreshButton.addTarget(self, action: #selector(refreshTapped), for: .touchUpInside)
-        setupCards()
     }
-    
+
     private func setupCustomImageLabel() {
         let imageView = UIImageView(image: UIImage(named: "redBackground"))
         imageView.contentMode = .scaleToFill
         view.addSubview(imageView)
-        
-        
+
         moviesLabel.text = "MOVIES: 0"
         moviesLabel.textColor = .white
         moviesLabel.font = UIFont(name: "Helvetica-Bold", size: 20)
-        moviesLabel.textAlignment = .left 
+        moviesLabel.textAlignment = .left
         imageView.addSubview(moviesLabel)
-        
-        
-        
+
         timeLabel.text = "TIME: 00:00"
         timeLabel.textColor = .white
         timeLabel.font = UIFont(name: "Helvetica-Bold", size: 20)
         timeLabel.textAlignment = .right
         imageView.addSubview(timeLabel)
-        
 
         imageView.snp.makeConstraints {
             $0.top.equalTo(settingButton.snp.bottom).offset(10)
@@ -168,120 +152,103 @@ class GameViewController: UIViewController {
             $0.leading.equalToSuperview().offset(20)
         }
 
-        
         timeLabel.snp.makeConstraints {
             $0.centerY.equalToSuperview()
             $0.trailing.equalToSuperview().offset(-20)
         }
     }
-    
-    private func updatePauseButtonImage() {
-        let imageName = viewModel.isPaused ? "playButton" : "pauseButton"
-        pauseButton.setImage(UIImage(named: imageName), for: .normal)
-    }
-    
-    private func showWinScreen(moviesCount: Int, timeString: String) {
-        let winVM = WinViewModel(
-            moviesCount: moviesCount,
-            timeString: timeString,
-            backgroundScene: skView.scene 
-        )
-        
-        let winVC = WinViewController(viewModel: winVM)
-        winVC.modalPresentationStyle = .overFullScreen
-        present(winVC, animated: true)
-    }
-   
-        
+
     private func setupCards() {
         let rows = 4
         let columns = 4
-        let spacing: CGFloat = 15
+        let spacing: CGFloat = 10
 
         cardButtons.removeAll()
         gridView.subviews.forEach { $0.removeFromSuperview() }
 
-        gridView.layoutIfNeeded()
-        let availableWidth = gridView.bounds.width - CGFloat(columns - 1) * spacing
-        let cellWidth = availableWidth / CGFloat(columns)
+        let mainStack = UIStackView()
+        mainStack.axis = .vertical
+        mainStack.spacing = spacing
+        mainStack.distribution = .fillEqually
 
-        for row in 0..<rows {
-            for column in 0..<columns {
-                let index = row * columns + column
+        for _ in 0..<rows {
+            let rowStack = UIStackView()
+            rowStack.axis = .horizontal
+            rowStack.spacing = spacing
+            rowStack.distribution = .fillEqually
+
+            for _ in 0..<columns {
                 let button = UIButton()
-                button.tag = index
                 button.setImage(UIImage(named: "closeSlot"), for: .normal)
                 button.layer.cornerRadius = 8
                 button.layer.borderWidth = 2
                 button.layer.borderColor = UIColor.white.cgColor
+                button.tag = cardButtons.count
                 button.addTarget(self, action: #selector(cardTapped(_:)), for: .touchUpInside)
-
-                gridView.addSubview(button)
                 cardButtons.append(button)
-
-                button.snp.makeConstraints { make in
-                    make.width.height.equalTo(cellWidth)
-                    if row == 0 {
-                        make.top.equalToSuperview()
-                    } else {
-                        make.top.equalTo(cardButtons[(row - 1) * columns + column].snp.bottom).offset(spacing)
-                    }
-                    if column == 0 {
-                        make.leading.equalToSuperview()
-                    } else {
-                        make.leading.equalTo(cardButtons[row * columns + column - 1].snp.trailing).offset(spacing)
-                    }
-                }
+                rowStack.addArrangedSubview(button)
             }
+
+            mainStack.addArrangedSubview(rowStack)
         }
+
+        gridView.addSubview(mainStack)
+        mainStack.snp.makeConstraints { $0.edges.equalToSuperview() }
+    }
+
+    private func updatePauseButtonImage() {
+        let imageName = viewModel.isPaused ? "playButton" : "pauseButton"
+        pauseButton.setImage(UIImage(named: imageName), for: .normal)
+    }
+
+    private func showWinScreen(moviesCount: Int, timeString: String) {
+        let winVM = WinViewModel(moviesCount: moviesCount, timeString: timeString, backgroundScene: skView.scene)
+        let winVC = WinViewController(viewModel: winVM)
+        winVC.modalPresentationStyle = .overFullScreen
+        present(winVC, animated: true)
     }
 
     private func bindViewModel() {
         viewModel.onMatchFound = { [weak self] first, second in
-            guard let self else { return }
-            self.cardButtons[first].alpha = 0.5
-            self.cardButtons[second].alpha = 0.5
+            self?.cardButtons[first].alpha = 0.5
+            self?.cardButtons[second].alpha = 0.5
         }
 
         viewModel.onMismatch = { [weak self] first, second in
-            guard let self else { return }
-            self.flipCard(at: first, faceUp: false)
-            self.flipCard(at: second, faceUp: false)
+            self?.flipCard(at: first, faceUp: false)
+            self?.flipCard(at: second, faceUp: false)
         }
-        
-        viewModel.onTimerUpdate = { minute,seconds in
-            self.timeLabel.text = String(format: "TIME: %02d:%02d", minute, seconds)
+
+        viewModel.onTimerUpdate = { [weak self] minute, seconds in
+            self?.timeLabel.text = String(format: "TIME: %02d:%02d", minute, seconds)
         }
-        
+
         viewModel.onGameReset = { [weak self] in
             self?.closeAllCards()
         }
-        
+
         viewModel.onMovesChanged = { [weak self] count in
             self?.moviesLabel.text = "MOVIES: \(count)"
         }
-        
+
         viewModel.onGameCompleted = { [weak self] movesCount, timeString in
             self?.showWinScreen(moviesCount: movesCount, timeString: timeString)
         }
     }
-    
-    // MARK: - Actions
+
     @objc private func settingTapped() {
         if SettingsManager.shared.isVibrationEnabled {
-            let generator = UIImpactFeedbackGenerator(style: .medium)
-            generator.impactOccurred()
+            UIImpactFeedbackGenerator(style: .medium).impactOccurred()
         }
         if SettingsManager.shared.isSoundEnabled {
             viewModel.playButtonClick()
         }
         viewModel.settingButtonTapped()
     }
-    
+
     @objc private func backTapped() {
         if SettingsManager.shared.isVibrationEnabled {
-            let generator = UIImpactFeedbackGenerator(style: .medium)
-            generator.impactOccurred()
+            UIImpactFeedbackGenerator(style: .medium).impactOccurred()
         }
         if SettingsManager.shared.isSoundEnabled {
             viewModel.playButtonClick()
@@ -291,21 +258,17 @@ class GameViewController: UIViewController {
 
     @objc private func cardTapped(_ sender: UIButton) {
         if SettingsManager.shared.isVibrationEnabled {
-            let generator = UIImpactFeedbackGenerator(style: .light)
-            generator.impactOccurred()
+            UIImpactFeedbackGenerator(style: .light).impactOccurred()
         }
         let index = sender.tag
-        let shouldFlip = viewModel.handleCardFlip(at: index)
-        
-        if shouldFlip {
+        if viewModel.handleCardFlip(at: index) {
             flipCard(at: index, faceUp: true)
         }
     }
-    
+
     @objc private func pauseTapped() {
         if SettingsManager.shared.isVibrationEnabled {
-            let generator = UIImpactFeedbackGenerator(style: .medium)
-            generator.impactOccurred()
+            UIImpactFeedbackGenerator(style: .medium).impactOccurred()
         }
         if SettingsManager.shared.isSoundEnabled {
             viewModel.playButtonClick()
@@ -313,35 +276,28 @@ class GameViewController: UIViewController {
         viewModel.togglePause()
         updatePauseButtonImage()
     }
-    
+
     @objc private func refreshTapped() {
         if SettingsManager.shared.isVibrationEnabled {
-            let generator = UIImpactFeedbackGenerator(style: .medium)
-            generator.impactOccurred()
+            UIImpactFeedbackGenerator(style: .medium).impactOccurred()
         }
         viewModel.resetGame()
         viewModel.startGameTimer()
     }
-    
+
     private func flipCard(at index: Int, faceUp: Bool) {
-        UIView.transition(with: cardButtons[index],
-                          duration: 0.3,
-                          options: .transitionFlipFromLeft,
-                          animations: {
+        UIView.transition(with: cardButtons[index], duration: 0.3, options: .transitionFlipFromLeft, animations: {
             self.cardButtons[index].setImage(
                 UIImage(named: faceUp ? self.viewModel.cards[index].imageName : "closeSlot"),
                 for: .normal
             )
         })
     }
-    
+
     private func closeAllCards() {
         for (index, button) in cardButtons.enumerated() {
             button.alpha = 1.0
-            UIView.transition(with: button,
-                              duration: 0.3,
-                              options: .transitionFlipFromRight,
-                              animations: {
+            UIView.transition(with: button, duration: 0.3, options: .transitionFlipFromRight, animations: {
                 button.setImage(UIImage(named: "closeSlot"), for: .normal)
             })
         }
